be3fc2796bf9202cd9f1126a38d953c16f552b84
ignotvvs <s221480@student.uek.krakow.pl>
2024-10-29 01:32:11 +0100

    add fixes

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,4 @@
+from datetime import date
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
 from lorem_text import lorem
@@ -6,26 +7,30 @@ posts_list = [
     {
         "id": "misty-esoterica",
         "title": "Misty's Esoterica",
-        "content": lorem.paragraphs(4),
-        "date_update": "10 Apr 2077",
-        "description": lorem.words(50),
-        "photo": "blog/images/misty.jpg"
+        "content": lorem.paragraphs(5),
+        "date_update": date(2077, 4, 10),
+        "description": lorem.words(20),
+        "photo": "misty.jpg"
     },{
         "id": "dogtown",
         "title": "Dogtown District",
-        "content": lorem.paragraphs(4),
-        "date_update": "20 Sep 2077",
+        "content": lorem.paragraphs(5),
+        "date_update": date(2077, 9, 20),
         "description": lorem.words(20),
-        "photo": "blog/images/dogtown.png"
+        "photo": "dogtown.png"
     }
 ]
 
 # Create your views here.
 
+def get_date(post_n):
+    return post_n['date_update']
+
 def index(request):
+    sorted_posts = sorted(posts_list,key=get_date)
     preview_posts = None
     if len(posts_list):
-        preview_posts = posts_list[0:3]
+        preview_posts = sorted_posts[-3:]
     return render(request, "blog/index.html", {"posts": preview_posts})
 
 def posts(request):
@@ -35,13 +40,9 @@ def posts(request):
 
 def post(request, slug):
     try:
-        sel_post =next(item for item in posts_list if item["id"] == slug)
+        sel_post = next(item for item in posts_list if item["id"] == slug)
         return render(request, "blog/post.html", {
-            "post_title": sel_post["title"],
-            "post_content": sel_post["content"],
-            "post_date": sel_post["date_update"],
-            "post_pic" : sel_post["photo"]
-
+            "post": sel_post
         })
-    except KeyError:
-        return HttpResponseNotFound("Wrong name")
+    except (KeyError, StopIteration):
+        return HttpResponseNotFound("wrong")
