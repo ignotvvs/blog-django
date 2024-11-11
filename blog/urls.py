da5f6172fee6cd332f45bfa3421000d1bf034c12
ignotvvs <s221480@student.uek.krakow.pl>
2024-11-11 02:09:54 +0100

    add read later

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.IndexView.as_view(), name='index'),
-    path("posts/<slug:slug>/add-comment", views.CommentFormView.as_view(), name="comment"),
     path("posts/", views.PostsListView.as_view(), name="posts"),
-    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post")
+    path("posts/<slug:slug>", views.PostDetailView.as_view(), name="post"),
+    path("posts/<slug:slug>/add-comment", views.CommentFormView.as_view(), name="comment"),
+    path("read-later", views.ReadLaterView.as_view(), name="read-later")
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.views.generic import ListView, DetailView
+from django.urls import reverse
 #from django.views.generic.edit import CreateView
 # from lorem_text import lorem
 from .models import Post
@@ -31,10 +32,11 @@ class IndexView(ListView):
     template_name = "blog/index.html"
     model = Post
     context_object_name = "posts"
+    ordering = ["-date"]
 
     def get_queryset(self):
         base_query = super().get_queryset()
-        data = base_query.order_by("-date")[:3]
+        data = base_query[:3]
         return data
 
 class PostsListView(ListView):
@@ -47,27 +49,68 @@ class PostDetailView(DetailView):
     template_name = "blog/post.html"
     model = Post
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        loaded_post = self.object
+        request = self.request
+        slugs = request.session.get("stored_posts")
+        if slugs and str(loaded_post.slug) in slugs:
+            context["is_saved"] = True
+        else:
+            context["is_saved"] = False
+        return context
+    
+
 class CommentFormView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request):
         form = CommentForm()
         return render(request, "blog/form.html", {
             "form": form
         })
     
-    def post(self, request, *args, **kwargs):
+    def post(self, request, slug):
         form = CommentForm(request.POST, request.FILES)
-        post_slug = self.kwargs.get("slug")
-        com_post = Post.objects.get(slug=post_slug)
+        com_post = Post.objects.get(slug=slug)
 
         if form.is_valid():
-            form.instance = form.save(commit=False)
-            form.instance.post = com_post
-            form.instance.save()
+            comment = form.save(commit=False)
+            comment.post = com_post
+            comment.save()
             return redirect(com_post.get_absolute_url())
 
         return render(request, "blog/form.html", {
             "form": form
         })
+    
+class ReadLaterView(View):
+    def get(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        context = {}
+
+        if stored_posts is None or len(stored_posts) == 0:
+            context["posts"] = []
+        else:
+            context["posts"] = Post.objects.filter(slug__in=stored_posts)
+
+        return render(request, "blog/stored-posts.html", context=context)
+
+    def post(self, request):
+        stored_posts = request.session.get("stored_posts")
+
+        if stored_posts is None:
+            stored_posts = []
+
+        post_slug = str(request.POST["post_slug"])
+
+        if post_slug not in stored_posts:
+            stored_posts.append(post_slug)
+        else:
+            stored_posts.remove(post_slug)
+
+        request.session["stored_posts"] = stored_posts
+        
+        return redirect(reverse("post", args=[post_slug]))
 
 # def index(request):
 #     preview_posts = Post.objects.all().order_by("-date")[:3]
